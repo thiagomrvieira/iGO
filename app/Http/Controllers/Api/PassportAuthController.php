b72782bceb3c792eeb618426f487d113b48e4384
Thiago Vieira <tmaciel@digital-works.com>
2021-09-01 14:27:41 +0100

    Add routes and cotroller for auth

---
+++ app/Http/Controllers/Api/PassportAuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PassportAuthController extends Controller
+{
+    /**
+     * Registration
+     */
+    public function register(Request $request)
+    {
+ 
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+       
+        $token = $user->createToken('igoApiToken')->accessToken;
+ 
+        return response()->json(['token' => $token], 200);
+    }
+ 
+    /**
+     * Login
+     */
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+ 
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('igoApiToken')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }   
+}

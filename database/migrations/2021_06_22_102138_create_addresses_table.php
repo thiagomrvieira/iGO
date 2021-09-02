42505f98a7d1fe56bfb862129404e37534ca0d77
Thiago Vieira <tmaciel@digital-works.com>
2021-09-02 10:44:11 +0100

    Change datatype in address migration

--- database/migrations/2021_06_22_102138_create_addresses_table.php
+++ database/migrations/2021_06_22_102138_create_addresses_table.php
@@ -15,7 +15,7 @@ class CreateAddressesTable extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->string('line_1');
             $table->string('line_2')->nullable();
             $table->string('county');

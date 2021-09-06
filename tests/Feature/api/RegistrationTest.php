6c120193b0c8c6e56468b0efc87f85d4966ce84f
Thiago Vieira <tmaciel@digital-works.com>
2021-09-06 11:16:29 +0100

    Add test for user trying to register with in use email

--- tests/Feature/api/RegistrationTest.php
+++ tests/Feature/api/RegistrationTest.php
@@ -14,8 +14,8 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post('/api/v1/register', [
-            'name'                => 'Test User 01',
-            'email'               => 'test01@example.com',
+            'name'                => 'Test User 02',
+            'email'               => 'test05@example.com',
             'mobile_phone_number' => 'password',
             'line_1'              => 'Rua Diogo Cão',
             'county'              => 'Estoril',
@@ -24,9 +24,31 @@ class RegistrationTest extends TestCase
 
         ]);
 
-        $response->assertStatus(200);
+        $response->assertSuccessful();
 
     }
 
+    public function test_users_can_not_register_with_in_use_email()
+    {
+        $response = $this->post('/api/v1/register', [
+            'name'                => 'Test User 02',
+            'email'               => 'test05@example.com',
+            'mobile_phone_number' => 'password',
+            'line_1'              => 'Rua Diogo Cão',
+            'county'              => 'Estoril',
+            'city'                => 'Cascais',
+            'password'            => 'password',
+
+        ]);
+
+        $response->assertInvalid(['email']);
+
+        $response->assertSessionHasErrors([
+            'email' => 'O email informado já está em uso.'
+        ]);
+
+        
+
+    }
 
 }

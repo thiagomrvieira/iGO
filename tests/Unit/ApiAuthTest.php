1ddff25eed1d8ad5bbe059bd4c1954b83f1778ed
Thiago Vieira <tmaciel@digital-works.com>
2021-09-07 11:35:34 +0100

    Finish register test

--- tests/Unit/ApiAuthTest.php
+++ tests/Unit/ApiAuthTest.php
@@ -8,28 +8,23 @@ use Tests\TestCase;
 
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
 
 class ApiAuthTest extends TestCase
 {
-    public $user;
-
-    protected function setUp() :void
-    {
-        parent::setUp();
-        $this->user = User::factory()->make();
-    }
+    use WithFaker;
 
     public function test_new_users_can_register()
     {
         $response = $this->post('/api/v1/register', [
-            'name'                => $this->user->name,
-            'email'               => $this->user->email,
-            'mobile_phone_number' => '987 654 321',
-            'line_1'              => 'Rua Diogo Cão',
-            'county'              => 'Estoril',
-            'city'                => 'Cascais',
-            'password'            => 'password',
-
+            'name'                => $this->faker->name(),
+            'email'               => $this->faker->unique()->safeEmail(),
+            'mobile_phone_number' => $this->faker->phoneNumber(),
+            'line_1'              => $this->faker->secondaryAddress(),
+            'county'              => $this->faker->cityPrefix(),
+            'city'                => $this->faker->city(),
+            'password'            => 'iGOdelivery',
         ]);
 
         $response->assertSuccessful();
@@ -40,29 +35,5 @@ class ApiAuthTest extends TestCase
             'token',
         ]);
     }
-
-    // public function test_users_can_not_register_with_in_use_email()
-    // {
-    //     $user = User::factory()->make();
-
-    //     $response = $this->post('/api/v1/register', [
-    //         'name'                => $this->user->name,
-    //         'email'               => $this->user->email,
-    //         'mobile_phone_number' => '987 654 321',
-    //         'line_1'              => 'Rua Diogo Cão',
-    //         'county'              => 'Estoril',
-    //         'city'                => 'Cascais',
-    //         'password'            => 'password',
-
-    //     ]);
-
-    //     $response->assertInvalid(['email']);
-
-    //     $response->assertSessionHasErrors([
-    //         'email' => 'O email informado já está em uso.'
-    //     ]);
-
-        
-
-    // }
+    
 }

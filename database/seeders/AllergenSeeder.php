39e4e427bf38f4da59294922d854bcf7a343a45b
Thiago Vieira <tmaciel@digital-works.com>
2021-09-10 11:25:12 +0100

    Add allergen seeder

--- database/seeders/AllergenSeeder.php
+++ database/seeders/AllergenSeeder.php
@@ -15,25 +15,18 @@ class AllergenSeeder extends Seeder
      */
     public function run()
     {
-        $partnerCategory = PartnerCategory::where('slug', 'alimentos')->first();
+        $restaurante = PartnerCategory::where('slug', 'restaurantes')->first();
 
         $data = array(
-            array('name'=>'Glúten',                        'category_id'=> $partnerCategory->id, 'slug' => 'gluten',                        'active' => true),
-            array('name'=>'Peixes',                        'category_id'=> $partnerCategory->id, 'slug' => 'peixes',                        'active' => true),
-            array('name'=>'Lactose',                       'category_id'=> $partnerCategory->id, 'slug' => 'lactose',                       'active' => true),
-            array('name'=>'Tremoço',                       'category_id'=> $partnerCategory->id, 'slug' => 'tremoco',                       'active' => true),
-            array('name'=>'Crustáceos',                    'category_id'=> $partnerCategory->id, 'slug' => 'crustaceos',                    'active' => true),
-            array('name'=>'Amendoins',                     'category_id'=> $partnerCategory->id, 'slug' => 'amendoins',                     'active' => true),
-            array('name'=>'Frutos de casca rija',          'category_id'=> $partnerCategory->id, 'slug' => 'frutos-de-casca-rija',          'active' => true),
-            array('name'=>'Sementes sésamo',               'category_id'=> $partnerCategory->id, 'slug' => 'sementes-sesamo',               'active' => true),
-            array('name'=>'Moluscos',                      'category_id'=> $partnerCategory->id, 'slug' => 'moluscos',                      'active' => true),
-            array('name'=>'Ovos',                          'category_id'=> $partnerCategory->id, 'slug' => 'ovos',                          'active' => true),
-            array('name'=>'Soja',                          'category_id'=> $partnerCategory->id, 'slug' => 'soja',                          'active' => true),
-            array('name'=>'Aipo',                          'category_id'=> $partnerCategory->id, 'slug' => 'aipo',                          'active' => true),
-            array('name'=>'Dióxido de enxofre e sulfitos', 'category_id'=> $partnerCategory->id, 'slug' => 'dioxido-de-enxofre-e-sulfitos', 'active' => true),
+            array('name'=>'Glúten',        'category_id'=> $restaurante->id, 'slug' => 'gluten',        'active' => true),
+            array('name'=>'Amendoim',      'category_id'=> $restaurante->id, 'slug' => 'amendoim',      'active' => true),
+            array('name'=>'Lactose',       'category_id'=> $restaurante->id, 'slug' => 'lactose',       'active' => true),
+            array('name'=>'Crustáceos',    'category_id'=> $restaurante->id, 'slug' => 'crustaceos',    'active' => true),
+            array('name'=>'Frutos do mar', 'category_id'=> $restaurante->id, 'slug' => 'frutos-do-mar', 'active' => true),
+            array('name'=>'Molusculos',    'category_id'=> $restaurante->id, 'slug' => 'molusculos',    'active' => true),
+            array('name'=>'Soja',          'category_id'=> $restaurante->id, 'slug' => 'soja',          'active' => true),
         );
         
         Allergen::insert($data);
-        
     }
 }

df430e0f1ca2d09c1385b086f1632caaee6a4655
Thiago Vieira <tmaciel@digital-works.com>
2021-09-13 15:16:49 +0100

    Add Subcategorias de Farmácias

--- database/seeders/PartnerCategorySeeder.php
+++ database/seeders/PartnerCategorySeeder.php
@@ -66,7 +66,7 @@ class PartnerCategorySeeder extends Seeder
 
         
         #   Cria categoria Lojas
-         $supermercado = PartnerCategory::create([
+         $loja = PartnerCategory::create([
             'name'      => 'Lojas',
             'slug'      => 'lojas',
             'parent_id' => null,
@@ -74,22 +74,40 @@ class PartnerCategorySeeder extends Seeder
         ]);
         #   Subcategorias de Lojas
         $data = array(
-            array('name'=>'Artigos do Lar',                      'parent_id'=> $supermercado->id, 'slug' => 'artigos-do-lar',                     'active' => true),
-            array('name'=>'Artigos de senhora',                  'parent_id'=> $supermercado->id, 'slug' => 'artigos-de-senhora',                 'active' => true),
-            array('name'=>'Artigos de homem',                    'parent_id'=> $supermercado->id, 'slug' => 'artigos-de-homem',                   'active' => true),
-            array('name'=>'Artigos de criança',                  'parent_id'=> $supermercado->id, 'slug' => 'artigos-de-crianca',                 'active' => true),
-            array('name'=>'Tecnologias & Electrónicos',          'parent_id'=> $supermercado->id, 'slug' => 'tecnologias-&-electronicos',         'active' => true),
-            array('name'=>'Artesanato e Roupa Africana',         'parent_id'=> $supermercado->id, 'slug' => 'artesanato-e-roupa-africana',        'active' => true),
-            array('name'=>'Bebidas alcoólicas',                  'parent_id'=> $supermercado->id, 'slug' => 'bebidas-alcoolicas',                 'active' => true),
-            array('name'=>'Bebidas sem álcool',                  'parent_id'=> $supermercado->id, 'slug' => 'bebidas-sem-alcool',                 'active' => true),
-            array('name'=>'Artigos de Festa',                    'parent_id'=> $supermercado->id, 'slug' => 'artigos-de-festa',                   'active' => true),
-            array('name'=>'Florista',                            'parent_id'=> $supermercado->id, 'slug' => 'florista',                           'active' => true),
-            array('name'=>'Amor, momentos e ocasiões especiais', 'parent_id'=> $supermercado->id, 'slug' => 'amor-momentos-e-ocasioes-especiais', 'active' => true),
-            array('name'=>'Artigos de higiene e biosegurança',   'parent_id'=> $supermercado->id, 'slug' => 'artigos-de-higiene-e-bioseguranca',  'active' => true),
-            array('name'=>'Agro',                                'parent_id'=> $supermercado->id, 'slug' => 'agro',                               'active' => true),
+            array('name'=>'Artigos do Lar',                      'parent_id'=> $loja->id, 'slug' => 'artigos-do-lar',                     'active' => true),
+            array('name'=>'Artigos de senhora',                  'parent_id'=> $loja->id, 'slug' => 'artigos-de-senhora',                 'active' => true),
+            array('name'=>'Artigos de homem',                    'parent_id'=> $loja->id, 'slug' => 'artigos-de-homem',                   'active' => true),
+            array('name'=>'Artigos de criança',                  'parent_id'=> $loja->id, 'slug' => 'artigos-de-crianca',                 'active' => true),
+            array('name'=>'Tecnologias & Electrónicos',          'parent_id'=> $loja->id, 'slug' => 'tecnologias-&-electronicos',         'active' => true),
+            array('name'=>'Artesanato e Roupa Africana',         'parent_id'=> $loja->id, 'slug' => 'artesanato-e-roupa-africana',        'active' => true),
+            array('name'=>'Bebidas alcoólicas',                  'parent_id'=> $loja->id, 'slug' => 'bebidas-alcoolicas',                 'active' => true),
+            array('name'=>'Bebidas sem álcool',                  'parent_id'=> $loja->id, 'slug' => 'bebidas-sem-alcool',                 'active' => true),
+            array('name'=>'Artigos de Festa',                    'parent_id'=> $loja->id, 'slug' => 'artigos-de-festa',                   'active' => true),
+            array('name'=>'Florista',                            'parent_id'=> $loja->id, 'slug' => 'florista',                           'active' => true),
+            array('name'=>'Amor, momentos e ocasiões especiais', 'parent_id'=> $loja->id, 'slug' => 'amor-momentos-e-ocasioes-especiais', 'active' => true),
+            array('name'=>'Artigos de higiene e biosegurança',   'parent_id'=> $loja->id, 'slug' => 'artigos-de-higiene-e-bioseguranca',  'active' => true),
+            array('name'=>'Agro',                                'parent_id'=> $loja->id, 'slug' => 'agro',                               'active' => true),
            
         );
         #   Cria Subcategorias de Lojas
+        PartnerCategory::insert($data);
+
+
+
+        #   Cria categoria Farmácias
+        $farmacia = PartnerCategory::create([
+            'name'      => 'Farmácias',
+            'slug'      => 'farmacias',
+            'parent_id' => null,
+            'active'    => 1,
+        ]);
+        #   Subcategorias de Farmácias
+        $data = array(
+            array('name'=>'Medicamentos sem prescrição médica', 'parent_id'=> $farmacia->id, 'slug' => 'medicamentos-sem-prescricao-medica', 'active' => true),
+            array('name'=>'Adulto',                             'parent_id'=> $farmacia->id, 'slug' => 'adulto',                             'active' => true),
+            array('name'=>'Criança',                            'parent_id'=> $farmacia->id, 'slug' => 'crianca',                            'active' => true),
+        );
+        #   Cria Subcategorias de Farmácias
         PartnerCategory::insert($data);
     }
 }

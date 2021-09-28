8172b63ba154238fe90130c06d9193e5b2cf6458
Thiago Vieira <tmaciel@digital-works.com>
2021-09-28 11:44:09 +0100

    Add model and controller for Cart

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -18,14 +21,90 @@ class OrderController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * CREATE A NEW ORDER
+     * *
+     * 
+     * @OA\Post(path="/api/v1/orders",
+     *   tags={"Orders"},
+     *   summary="Create a new order",
+     *   description="Create a new order - Expect to recieve a product id and a quantity",
+     *   operationId="createorder",
+     *   @OA\RequestBody(
+     *      required=true,
+     *      @OA\JsonContent(
+     *          type="object",
+     *          @OA\Property(property="product_id", type="integer", example="1"),
+     *          @OA\Property(property="quantity", type="integer", example="2"),
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *      description="Unauthenticated"
+     *   ),
+     *   @OA\Response(
+     *      response=400, 
+     *      description="Bad request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *   @OA\Response(
+     *      response=403,
+     *      description="Forbidden"
+     *   ),
+     *   security={
+     *     {"api_key": {}}
+     *   }
+     * )
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request['client_id'] = Auth::user()->client->id;
+
+        $order = Order::firstOrCreate(
+            array(
+                'client_id'            => Auth::user()->client->id,
+                'order_status_type_id' => 1,
+            ),
+            array(
+                $request->all()
+            ),
+        );
+        
+
+        return 'teste';
+        // $productAndQuantity    = $order->products->pluck('product_id')->toArray() ?? [];
+        // $productsIdsForUpdate [] = $request->product_id;
+
+        // $productAndQuantity = array_fill_keys(
+        //     $productsIdsForUpdate, 
+        //     array('quantity' => $request->quantity,)
+        // );
+
+        // foreach ($variable as $key => $value) {
+        //     # code...
+        // }
+
+        // $order->products()->sync($productAndQuantity);
+        $order->products()->sync([
+        
+                
+        ]);
+
+        return response()->json(['status'  => $status  ?? 'success',
+                                 'message' => $message ?? 'Produto adicionado ao carrinho',
+                                 'data'    => $order], 200); 
     }
 
     /**

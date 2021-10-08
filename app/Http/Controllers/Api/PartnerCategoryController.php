c515f2bed016f3d6099e0bfc0146f77994064712
Thiago Vieira <tmaciel@digital-works.com>
2021-10-08 10:19:20 +0100

    Create controller and add image column in migration

---
+++ app/Http/Controllers/Api/PartnerCategoryController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\PartnerCategory;
+use Illuminate\Http\Request;
+
+class PartnerCategoryController extends Controller
+{
+    /**
+     * Return a list of all active partners categories
+     * *
+     * @OA\Get(path="/api/v1/categories",
+     *   tags={"Partner Categories"},
+     *   summary="Get the list of categories",
+     *   description="Return a list of all active partners categories",
+     *   operationId="getListOfCategories",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *      description="Unauthenticated"
+     *   ),
+     *   @OA\Response(
+     *      response=400, 
+     *      description="Bad request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="Not found"
+     *   ),
+     *   @OA\Response(
+     *      response=403,
+     *      description="Forbidden"
+     *   ),
+     *   security={
+     *     {"api_key": {}}
+     *   }
+     * )
+     *
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return PartnerCategory::where('active', true)->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

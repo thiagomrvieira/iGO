f5c0ed5cac6e1ca6325160fa5b3e385a73e02f62
Thiago Vieira <tmaciel@digital-works.com>
2021-10-14 13:16:23 +0100

    Add the calc of shipping fee

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -45,4 +45,48 @@ class Order extends Model
     {
         return $this->belongsTo(Address::class);
     }
+
+
+    /**
+     * Return the subtotal - Sum of all product and extras in the order
+     */
+    public function subtotal()
+    {
+        $subTotal = 0;
+        
+        foreach ($this->cart as $cart) {
+            $subTotal += $cart->amount();
+        }
+        return $subTotal;
+    }
+
+    /**
+     * Return the total - Sum of Total and Shipping fee
+     */
+    public function total()
+    {
+        return $this->subTotal() + $this->shippingFee();
+    }
+
+    /**
+     * Return the shipping fee based in user and partner location
+     */
+    public function shippingFee()
+    {
+        # Get the first Product in the cart
+        $cart = $this->cart->first();
+        # Get the Partner
+        $partner = $cart->product->partner;
+        # Get The county names
+        $deliveryFrom = County::where('name', $this->address->county)->first();
+        $deliveryTo   = County::where('name', $partner->address->county)->first();
+
+        return ShippingFee::where('delivery_from', $deliveryFrom->id)
+                          ->where('delivery_to',   $deliveryTo->id)->first()->price;
+    }
+
+
+
+
+
 }

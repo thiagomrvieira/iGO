92fef5733d2e1ebf3b6a0e9bd3a4dcab5f051136
Thiago Vieira <tmaciel@digital-works.com>
2021-11-08 10:20:59 +0000

    Create order controller

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,8 @@ Route::group(['prefix' => 'v1'], function()
             'shippingfees' => ShippingFeeController::class,
             
         ]);
-
+        
+        #   CLIENT ENDPOINTS
         Route::group(['prefix' => 'client'], function() 
         {
             
@@ -96,6 +97,13 @@ Route::group(['prefix' => 'v1'], function()
             #   Show partner products
             Route::get('partners/{id}/products',    [PartnerController::class, 'showProducts' ]);
 
+        });
+
+        #   DELIVERYMAN ENDPOINTS
+        Route::group(['prefix' => 'deliveryman'], function() 
+        {
+            #   ORDERS
+            Route::get('orders', [ClientOrderController::class, 'index']);
         });
         
         Route::get('shippingfees/{from}/{to}',  [ShippingFeeController::class, 'showByFromTo' ]);
